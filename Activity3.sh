8b86e3216cdbe47cf755599ad94b4d01553baa6d
Galima Patrick Ace <patrickace.galima@neu.edu.ph>
2025-02-12 07:34:15 +0000

    done

---
+++ Activity3.sh
@@ -0,0 +1,21 @@
+echo "Enter your Firstname:"
+read FIRSTNAME
+
+echo "Enter you last name:"
+read LASTNAME
+
+echo "Enter your Age:"
+read AGE
+
+echo "Enter your BirthDate format(mm/dd/yyyy):"
+read BIRTHDATE
+
+echo "Enter your Address:"
+read ADDRESS
+echo "-----------------------------"
+echo "Hi, $FIRSTNAME $LASTNAME"
+echo "Age:$AGE"
+echo "BirthDate(mm/dd/yyyy):$BIRTHDATE"
+echo "Address:$ADDRESS"
+
+
