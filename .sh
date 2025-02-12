2047630a114ad42298b6c79b353490c98501b6e2
Galima Patrick Ace <patrickace.galima@neu.edu.ph>
2025-02-12 00:52:42 +0000

    done

---
+++ .sh
@@ -0,0 +1,10 @@
+echo "Enter your name:"
+read Name
+
+echo "Enter your age:"
+read AGE
+
+echo "Enter your Favorite programming language"
+read LANGUAGE
+
+echo "Thankyou $NAME, You are $AGE years old and your favorite programming language is $LANGUAGE"
